27e4c38810cf5f848a06f9b361a2b8e563722d9d
Georgiy051188 <Geogiy88@gmail.com>
2019-06-15 14:01:46 +0300

    Priority Turn

--- src/PriorityTurn.java
+++ src/PriorityTurn.java
@@ -1,2 +1,12 @@
+import java.util.ArrayList;
+
 public class PriorityTurn {
+    private ArrayList<HuffmanTree> data;
+    private int nItems;
+
+    public PriorityTurn () {
+        data = new ArrayList<HuffmanTree>();
+        nItems = 0;
+    }
+
 }
