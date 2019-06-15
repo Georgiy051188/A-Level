07c9181b976004aa8685d01709f73566759f3d17
Georgiy051188 <Geogiy88@gmail.com>
2019-06-15 13:57:19 +0300

    Priority Turn

--- src/HuffmanNode.java
+++ src/HuffmanNode.java
@@ -1,2 +1,51 @@
-public class HuffmanNode {
+import org.w3c.dom.Node;
+
+public class Node {
+    private int frequence;//частота
+    private char letter;//буква
+    private Node leftChild;//левый потомок
+    private Node rightChild;//правый потомок
+
+
+
+    public Node(char letter, int frequence) {
+        this.letter = letter;
+        this.frequence = frequence;
+    }
+
+    public Node() {}
+    public void addChild(Node newNode) {
+        if (leftChild == null)
+            leftChild = newNode;
+        else {
+            if (leftChild.getFrequence() <= newNode.getFrequence())
+                rightChild = newNode;
+            else {
+                rightChild = leftChild;
+                leftChild = newNode;
+            }
+        }
+
+        frequence += newNode.getFrequence();
+    }
+
+    public Node getLeftChild() {
+        return leftChild;
+    }
+
+    public Node getRightChild() {
+        return rightChild;
+    }
+
+    public int getFrequence() {
+        return frequence;
+    }
+
+    public char getLetter() {
+        return letter;
+    }
+
+    public boolean isLeaf() {
+        return leftChild == null && rightChild == null;
+    }
 }
--- src/HuffmanTree.java
+++ src/HuffmanTree.java
@@ -1,2 +1,23 @@
 public class HuffmanTree {
+    private Node root;
+
+    public HuffmanTree() {
+        root = new Node();
+
+    }
+
+    public HuffmanTree(Node root) {
+        this.root = root;
+
+    }
+
+    public int getFrequence() {
+        return root.getFrequence();
+
+    }
+
+    public Node getRoot() {
+        return root;
+
+    }
 }
